03ca495cc6ea89b027584692aee91d7efb0ba474
sushil sharma <sharma.s420823@gmail.com>
2024-09-25 19:07:28 +0545

    -

--- class_variables/main.py
+++ class_variables/main.py
@@ -2,7 +2,18 @@ from students import Students
 
 student1=Students("Person1",32)
 student2=Students("Person2",23)
+student3=Students("Person3",12)
+# student4=Students("Person4",20)
 
-print(student1.age)
-print(student2.age)
-print(Students.pass_year)
+
+
+
+
+# print(student1.age)
+# print(student2.age)
+# print(Students.pass_year)
+
+
+print(Students.students_no)
+
+print(f"No of students in {Students.pass_year} is {Students.students_no}")
--- class_variables/students.py
+++ class_variables/students.py
@@ -1,7 +1,9 @@
 class Students:
 
-    pass_year=2024
+    pass_year=2026
+    students_no=0
 
     def __init__(self,name,age):
         self.name=name
         self.age=age
+        Students.students_no+=1
