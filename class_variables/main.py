ae8a14e53e39009eb08f4b27ea0af044c549c0e7
sushil sharma <sharma.s420823@gmail.com>
2024-09-25 18:59:34 +0545

    -

---
+++ class_variables/main.py
@@ -0,0 +1,8 @@
+from students import Students
+
+student1=Students("Person1",32)
+student2=Students("Person2",23)
+
+print(student1.age)
+print(student2.age)
+print(Students.pass_year)
---
+++ class_variables/students.py
@@ -0,0 +1,7 @@
+class Students:
+
+    pass_year=2024
+
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
