ee42718a6236a5a9fc346d86302e05045bba0ecd
sushil sharma <sharma.s420823@gmail.com>
2024-09-28 19:40:27 +0545

    -

--- 12. File detection/main.py
+++ 12. File detection/main.py
@@ -1,8 +1,8 @@
 import os
 
 
-file_path="test.txt"
-print(os.getcwd())
+file_path="12. File detection/test.txt"
+# print(os.getcwd())
 
 if os.path.exists(file_path):
     print(f"The provided path '{file_path}' exists ")
