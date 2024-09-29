c6711ce5692a845fdb6ad79801c0d0b7bfe24bb2
sushil sharma <sharma.s420823@gmail.com>
2024-09-29 18:52:34 +0545

    -

--- 13. File reading/main.py
+++ 13. File reading/main.py
@@ -1,21 +1,35 @@
 # txt_data="Hello there !!!"
 
 import json
-data={
-    "name":"void",
-    "age":90,
-    "type":"galaxy"
-}
+import csv
+
+# data={
+#     "name":"void",
+#     "age":90,
+#     "type":"galaxy"
+# }
 
 # txt_data=["this","is","just","a","collection"]
-file_path="C:/Users/DELL/Desktop/hello.json"
+
+csv_data=[
+    ["Name","Age","Job"],
+    ["Void",200,"NOthing"],
+    ["Null",300,"Anything"],
+    ["Exit",100,"Here"]
+]
+
+
+file_path="C:/Users/DELL/Desktop/hello.csv"
 
 try:
-    with open(file_path,"w")as file:
-        json.dump(data,file,indent=4)
+    with open(file_path,"w",newline="")as file:
+        writer=csv.writer(file)
+        for row in csv_data:
+            writer.writerow(row)
+        # json.dump(data,file,indent=4)
         # for name in txt_data:
         #     file.write(name+"\n")
-        print(f"Json file at {file_path} was created")
+        print(f"CSV file at {file_path} was created")
 
 except FileExistsError:
     print("File already exists")
