47409c86d62c57b6a02912538f06bcac5b56bce1
sushil sharma <sharma.s420823@gmail.com>
2024-09-30 15:07:36 +0545

    -

--- 16. Alaram clock/main.py
+++ 16. Alaram clock/main.py
@@ -1,17 +1,32 @@
 import datetime
+import time
+import pygame
 
 class Alarm:
-    def __init__(self,hour,min):
+    def __init__(self,hour,min,sec):
         self.hour=hour
         self.min=min
+        self.sec=sec
 
     def start_timer(self):
-        current=datetime.datetime.now()
-        if current.hour==self.hour and current.minute == self.min:
-            print("it's time")
-        else:
-            print(current)
-            print("no time")
+        alarm=f"{self.hour:02}:{self.min:02}:{self.sec:02}"
+        run=True
+        while run:
+            current_time=datetime.datetime.now().strftime("%H:%M:%S")
+            print(current_time)
+            if(current_time==alarm):
+                print("Wake UP🚘🚘🚘🚔🚑")            
+                run=False
+            time.sleep(1)
 
-alarm=Alarm(13,45)
-alarm.start_timer()
+        # current=datetime.datetime.now()
+        # if current.hour==self.hour and current.minute == self.min:
+        #     print("it's time")
+        # else:
+        #     print(current)
+        #     print("no time")
+
+alarm=input("Enter the time to wake up (HH:MM:SS): ")
+houred,minut,sec=map(int,alarm.split(":"))
+alarmed=Alarm(houred,minut,sec)
+alarmed.start_timer()
