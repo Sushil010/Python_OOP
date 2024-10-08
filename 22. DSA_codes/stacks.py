05a97cd9b82a50139ebf430dcb8f5abe8dc3d2c6
sushil sharma <sharma.s420823@gmail.com>
2024-10-08 22:41:37 +0545

    -

--- 22. DSA_codes/stacks.py
+++ 22. DSA_codes/stacks.py
@@ -1,3 +1,39 @@
 class stacks:
-    def __init__(self,num):
-        self.num=num
+    lists=[]
+    # def __init__(self,num):
+    #     self.num=num
+
+    # def push(self):
+    #     stacks.lists.append(self.num)
+    #     print(f"Number {self.num} has been pushed into stack")
+
+    # def poper(self):
+    #     popped_no=stacks.lists.pop()
+    #     print(f"Number {popped_no} has been popped out")
+
+    def __init__(self):
+        pass
+
+    def push(self,num):
+        for nums in num:
+            stacks.lists.append(nums)
+            print(f"Number {nums} has been appended")
+
+    def poper(self,iter):
+        for _ in range(iter):
+            popped_no =stacks.lists.pop()
+            print(f"Number {popped_no} has been popped out")
+
+
+   
+
+
+Stacks=stacks()
+Stacks.push([1,2,4,3,5])
+Stacks.poper(3)
+# Stacks.poper()
+# Stacks.poper()
+# Stacks.poper()
+print(Stacks.lists)
+
+
