3271ebef6f26975e653bd9f77a46a799c9eb11c0
Sushil Sharma <sharma.s420823@gmail.com>
2024-12-25 16:53:40 +0545

    -

--- Prac/super.py
+++ Prac/super.py
@@ -4,6 +4,12 @@ class Parent1:
         self.color=color
 
 class children1(Parent1):
-    def first(self,height):
+    def __init__(self,name,color,height):
         super().__init__(name,color)
         self.height=height
+
+
+# flow is first paramater reach to children and only to parent
+# useful when parameters need to change before passing to Parent class
+
+c1=children1("circle","green",20)
