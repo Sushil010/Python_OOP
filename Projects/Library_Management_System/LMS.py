fe4626f55dee37b908ca2526dce9a5af49711d0e
Sushil Sharma <sharma.s420823@gmail.com>
2024-12-31 22:46:45 +0545

    class_book and readme

--- Projects/Library_Management_System/LMS.py
+++ Projects/Library_Management_System/LMS.py
@@ -1,1 +1,16 @@
 print("Library Management System")
+
+class Book:
+    def __init__(self,title,author,id,copies):
+        self.title=title
+        self.author=author
+        self.id=id
+        self.copies=copies
+    
+    # changes--provide the book name according to id of the book
+    def display_details(self):
+        print(f"Book name is {self.title} and author is {self.author}")
+
+    def update_copies(self):
+        pass
+
---
+++ Projects/Library_Management_System/README.md
@@ -0,0 +1,11 @@
+Book
+
+Attributes:
+book_id (unique identifier)
+title
+author
+copies_available
+
+Methods:
+display_details() - Show book information.
+update_copies(count) - Increment or decrement the copies.
