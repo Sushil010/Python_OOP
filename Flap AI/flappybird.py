d88c0d1bac2527884b22a2d6112806438e94ff7c
Sushil Sharma <sharma.s420823@gmail.com>
2025-01-03 15:40:12 +0545

    image_load

--- Flap AI/flappybird.py
+++ Flap AI/flappybird.py
@@ -6,3 +6,22 @@ import neat
 import time
 import random
 
+WIN_HEIGHT=600
+WIN_WIDTH=800
+
+Bird_img=[
+    
+pygame.transform.scale2x(
+pygame.image.load(os.path.join('Flap AI\imgs','bird1.png'))
+                        ),
+
+pygame.transform.scale2x(
+pygame.image.load(os.path.join('Flap AI\imgs','bird1.png'))
+                        ),
+
+pygame.transform.scale2x(
+pygame.image.load(os.path.join('Flap AI\imgs','bird1.png'))
+                        )
+
+]
+
