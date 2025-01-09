62a96dad5b5634343bbf8302a276e7dc29806c1e
Sushil Sharma <sharma.s420823@gmail.com>
2025-01-09 18:16:53 +0545

    pipes_add_final

--- Projects/Library_Management_System/neural/main.py
+++ Projects/Library_Management_System/neural/main.py
@@ -2,10 +2,13 @@ import pygame
 from sys import exit
 import os
 import config
-
+import components
 
 pygame.init()
 clock=pygame.time.Clock()
+
+def generate_pipes():
+    config.pipes.append(components.Pipes(config.win_width))
 
 def quit_game():
     for event in pygame.event.get():
@@ -14,10 +17,24 @@ def quit_game():
             exit()
 
 def main():
+    pipes_spawn_time=10
     while True:
         quit_game()
         config.win.fill((0,0,0))
+
         config.ground.draw(config.win)
+
+        if pipes_spawn_time<=0:
+            generate_pipes()
+            pipes_spawn_time=200
+        
+        pipes_spawn_time-=1
+
+        for pipe in config.pipes:
+            pipe.draw(config.win)
+            pipe.update()
+            if pipe.off_screen:
+                config.pipes.remove(pipe)
         
         clock.tick(60)
         pygame.display.flip()
